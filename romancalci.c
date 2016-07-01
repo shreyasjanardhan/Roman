f10d61b00d9b618d48e53ee232c15d4e7a051ea2
Shreyas Janardhan <shreyas.janardhan@mavs.uta.edu>
2016-07-01 00:55:21 -0500

    First Commit

---
+++ romancalci.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+  int main()
+{
+ char roman1[1000];
+ char roman2[1000],result[1000];
+printf("enter the first numeral:");
+scanf("%s",roman1);
+printf("\nenter the second numeral:");
+scanf("%s",roman2);
+return 0;
+}
+
